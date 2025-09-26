94ca05363e47772d81f34859369ae1e02d6b68f0
Lucas Bolens <lbolens@fu-r8-p8.s19.be>
2025-09-26 12:16:29 +0200

    doit gerer les quotes

--- parsing/expansion/variable_detection.c
+++ parsing/expansion/variable_detection.c
@@ -6,7 +6,7 @@
 /*   By: lbolens <lbolens@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/24 11:43:46 by lbolens           #+#    #+#             */
-/*   Updated: 2025/09/25 11:45:11 by lbolens          ###   ########.fr       */
+/*   Updated: 2025/09/26 11:47:55 by lbolens          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,10 +102,99 @@ char	*extract_variable(char *str)
 	return (variable);
 }
 
-void	expansion(t_cmd *commands, char **env)
+char	*extract_chain(char *str, int start, int end)
 {
+	int		size;
+	char	*result;
+	int		i;
+
+	size = end - start;
+	result = malloc((size + 1) * sizeof(char));
+	i = 0;
+	if (!result)
+		return (NULL);
+	while (start < end)
+	{
+		result[i] = str[start];
+		i++;
+		start++;
+	}
+	result[i] = '\0';
+	return (result);
+}
+
+char	*build_full_command(char *original, char **env, int exit_status)
+{
+	char	*result;
 	char	*variable;
 	char	*in_env;
+	size_t	i;
+	int		result_pos;
+	size_t	j;
+	int		k;
+	char	*status_str;
+
+	result = malloc(4096 * sizeof(char));
+	i = 0;
+	result_pos = 0;
+	if (!result)
+		return (NULL);
+	while (i < ft_strlen_pars(original))
+	{
+		if (original[i] == '$')
+		{
+			j = i + 1;
+			while (j < ft_strlen_pars(original) && ((original[j] >= 'a'
+						&& original[j] <= 'z') || (original[j] >= 'A'
+						&& original[j] <= 'Z') || (original[j] >= '0'
+						&& original[j] <= '9') || original[j] == '_'))
+				j++;
+			variable = extract_chain(original, i + 1, j);
+			if (ft_strcmp_pars(variable, "?") == 0)
+			{
+				status_str = ft_itoa_pars(exit_status);
+				k = 0;
+				while (status_str[k])
+				{
+					result[result_pos] = status_str[k];
+					result_pos++;
+					k++;
+				}
+				free(status_str);
+				free(variable);
+				i = j;
+			}
+			else
+			{
+				in_env = check_in_env(variable, env);
+				if (in_env != NULL)
+				{
+					k = 0;
+					while (in_env[k])
+					{
+						result[result_pos] = in_env[k];
+						result_pos++;
+						k++;
+					}
+				}
+				free(variable);
+				i = j;
+			}
+		}
+		else
+		{
+			result[result_pos] = original[i];
+			result_pos++;
+			i++;
+		}
+	}
+	result[result_pos] = '\0';
+	return (result);
+}
+
+void	expansion(t_cmd *commands, char **env, int exit_status)
+{
+	char	*expanded;
 	int		i;
 
 	while (commands)
@@ -115,61 +204,48 @@ void	expansion(t_cmd *commands, char **env)
 		{
 			if (is_variable(commands->args[i]))
 			{
-				variable = extract_variable(commands->args[i]);
-				if ((in_env = check_in_env(variable, env)) != NULL)
-					replace_in_command(commands, in_env, i);
-				else
-					replace_in_command(commands, "", i);
-				free(variable);
+				expanded = build_full_command(commands->args[i], env,
+						exit_status);
+				if (expanded)
+				{
+					replace_in_command(commands, expanded, i);
+					free(expanded);
+				}
 			}
 			i++;
 		}
-		// Expansion input_file (UNE SEULE FOIS par commande)
 		if (commands->input_file && is_variable(commands->input_file))
 		{
-			variable = extract_variable(commands->input_file);
-			if ((in_env = check_in_env(variable, env)) != NULL)
+			expanded = build_full_command(commands->input_file, env,
+					exit_status);
+			if (expanded)
 			{
 				free(commands->input_file);
-				commands->input_file = ft_strdup_pars(in_env);
+				commands->input_file = ft_strdup_pars(expanded);
+				free(expanded);
 			}
-			else
-			{
-				free(commands->input_file);
-				commands->input_file = ft_strdup_pars("");
-			}
-			free(variable);
 		}
-		// Même logique pour output_file et heredoc_delim...
 		if (commands->output_file && is_variable(commands->output_file))
 		{
-			variable = extract_variable(commands->output_file);
-			if ((in_env = check_in_env(variable, env)) != NULL)
+			expanded = build_full_command(commands->output_file, env,
+					exit_status);
+			if (expanded)
 			{
 				free(commands->output_file);
-				commands->output_file = ft_strdup_pars(in_env);
+				commands->output_file = ft_strdup_pars(expanded);
+				free(expanded);
 			}
-			else
-			{
-				free(commands->output_file);
-				commands->output_file = ft_strdup_pars("");
-			}
-			free(variable);
 		}
 		if (commands->heredoc_delim && is_variable(commands->heredoc_delim))
 		{
-			variable = extract_variable(commands->heredoc_delim);
-			if ((in_env = check_in_env(variable, env)) != NULL)
+			expanded = build_full_command(commands->heredoc_delim, env,
+					exit_status);
+			if (expanded)
 			{
 				free(commands->heredoc_delim);
-				commands->heredoc_delim = ft_strdup_pars(in_env);
+				commands->heredoc_delim = ft_strdup_pars(expanded);
+				free(expanded);
 			}
-			else
-			{
-				free(commands->heredoc_delim);
-				commands->heredoc_delim = ft_strdup_pars("");
-			}
-			free(variable);
 		}
 		commands = commands->next;
 	}
